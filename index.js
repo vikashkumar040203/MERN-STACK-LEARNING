efd379e43992887195856c33bab7c17f6f6193ce
vikashkumar040203 <vikashkumar990523@gmail.com>
2022-06-04 23:02:58 +0530

    mern

---
+++ index.js
@@ -0,0 +1,191 @@
+// alert("hello khushi");
+// document.getElementById("first").innerHTML = "khushi I love  U";
+
+// statement and comment
+// operators
+
+//   condition : if & else
+
+// alert . prompt , confirm
+
+// alert("alert function");
+// prompt("enter Your age");
+// confirm("are Your sure want to delete");
+
+// type consversion
+
+// let type = 4;
+// console.log(typeof type);
+
+// string manipulation
+
+// array
+// operation on an array
+
+// let book=['maths','bio',12,"english","dsa"];
+// book.push("khushi");
+
+// console.log(book.splice(0,1));
+// console.log(book.length());
+
+// function
+
+// user define funcction
+
+// multipliaction code
+
+// function multiplication(num){
+//     for( i=1;i<=10;i++){
+//         document.write(`${num}x ${i}=${num*i}`);
+//         document.write("<br>");
+
+//     }
+
+// }
+// multiplication(3);
+// document.write("<br>");
+// document.write("<br>");
+// multiplication(31);
+// document.write("<br>");
+// multiplication(322);
+// parameter and argument and argument object
+
+// function add(){
+//     if(arguments.length ==0){
+//         console.log("no parameter available");
+
+//     }
+//     else
+//     {
+//         let sum=0;
+//         for(let i=0;i<arguments.length;i++){
+//             sum+=arguments[i];
+//         }
+//         console.log(sum);
+//     }
+
+// }
+
+// add(1,2);
+// add(1,2);
+// add(1,2,2,22,23,566,-123);
+
+// return in function
+
+// function add(a,b){
+//     return a+b;
+// }
+// let c=add(1,2);
+// console.log(c);
+
+// anonymous function
+// function expression
+// let show = function(){
+//     console.log("khushi luv u");
+// }
+// show();
+
+// set timeout function
+// function show()
+// {
+//     console.log("khushi i luv u")
+// }
+// setTimeout(show,3000);
+
+// immediately invoked function
+
+// (function(){
+//     console.log("hello vikash");
+//     alert("hey khushi");
+// })();
+
+// object in js and properties of object
+
+// let person = {
+//     firstName:"vikku",
+//     loveLife:"khushi",
+//     MOb:93152222,
+//     sayHello(){
+//         console.log("hello"+car.brand+"car");
+//     }
+// };
+// console.log(vikash.loveLife);
+
+// person.sayHello=function()
+// {
+//     console.log("hello ")
+// }
+
+// person.sayHello();
+
+// or
+
+// function greet(){
+//     console.log("good morning");
+// }
+// person.sayHello= greet;
+
+// this key word
+
+// let car={
+//     brand:"tata",
+//     model:"safari"
+// }
+
+// person.sayHello();
+
+// maths Object, genrate random number
+
+// console.log(Math.random());
+
+// date <object data="" type=""></object>
+
+// let x= new Date();
+// console.log(x);
+
+// new key word
+// getter and setter
+
+// var person=
+// {
+//     name:"vikash",
+//     salary:1234,
+//     semester:"V"
+
+// }
+// console.log()
+
+// object constructor
+
+// function Student(first, last, age, sem) {
+//   this.firstname = first;
+//   this.lastname = last;
+//   this.age = age;
+//   this.sem = sem;
+// }
+// var st1 = new Student("khushi", "gupta", 19, 4);
+// console.log(st1);
+
+// object prototype
+
+// nested Object
+
+// Hoisting
+
+// hoist mean to lift or pull
+
+// hello();
+// function hello(){
+//     console.log("hello world");
+// }
+
+// console.log("after declarelation function")
+
+// console.log(x);
+// var x;
+// x=7;
+
+// DOM
+
+// select element by id
+
